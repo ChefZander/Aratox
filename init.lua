2e9e19a6401b07f1c6d5d135acd5d14d94aad3e4
Zander200-glitch <60350322+Zander200-glitch@users.noreply.github.com>
2021-04-16 21:45:04 +0200

    a

--- init.lua
+++ init.lua
@@ -1,9 +1,15 @@
 local modpath = minetest.get_modpath("aratox")
 
 local modules = {
+--autihandle
 "autohandle", 
+
+--speed
 "checks/speed-1", 
+
+--fly
 --"checks/fly-1"
+"checks/fly-2",
 }
 
 for _, module in pairs(modules) do
