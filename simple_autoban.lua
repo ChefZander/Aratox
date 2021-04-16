f56378ff06813f929ef42e3e161f964c3ddbd840
Zander200-glitch <60350322+Zander200-glitch@users.noreply.github.com>
2021-04-16 20:03:59 +0200

    Patch try #2

--- simple_autoban.lua
+++ simple_autoban.lua
@@ -1,3 +1,3 @@
-minetest.register_on_cheat(function(player, cheat){
-    minetest.ban_player(player:get_name(), "Aratox caught you cheating! (AntiCheat)")
-})
+minetest.register_on_cheat(function(player, cheat)
+    minetest.ban_player(player:get_player_name(), "a")
+)
