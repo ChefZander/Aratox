9fa1df8402eaccfce2a3aca7604943ddbe62c68a
Zander200-glitch <60350322+Zander200-glitch@users.noreply.github.com>
2021-04-16 20:37:14 +0200

    Improve autobans #2

--- simple_autoban.lua
+++ simple_autoban.lua
@@ -1,3 +1,3 @@
 minetest.register_on_cheat(function(player, cheat)
-    minetest.registered_chatcommands["ban"].func(player:get_player_name(), "Aratox caught you cheating! (AntiCheat)")
+    minetest.kick_player(player:get_player_name(), "Aratox caught you cheating! (AntiCheat)")
 end)
