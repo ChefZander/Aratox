15b78cf13c8a1078ea05d3ab0a5fe9d0a1b563d9
Zander200-glitch <60350322+Zander200-glitch@users.noreply.github.com>
2021-04-16 20:44:56 +0200

    Add antifly

---
+++ antifly.lua
@@ -0,0 +1,33 @@
+local function fly_check(player)
+	local pos = player:get_pos()
+	if not pos then return true end
+	if minetest.check_player_privs(player:get_player_name(), {fly = true}) then
+		return true
+	end
+	if minetest.get_node(pos).name == "air" and minetest.get_node({x = pos.x, y = pos.y - 1, z = pos.z}).name == "air" then
+		local airnodes = minetest.find_nodes_in_area({x = pos.x - 2, y = pos.y - 2, z = pos.z - 2}, {x = pos.x + 2, y = pos.y + 2, z = pos.z + 2}, {"air"})
+		local v = player:get_player_velocity()
+		if #airnodes >= 125 and v.y >= 0 then
+			return false
+		end
+	end
+
+	return true
+end
+
+minetest.register_globalstep(function(dtime)
+	local players = minetest.get_connected_players()
+	for _, player in ipairs(players) do
+		local name = player:get_player_name()
+		local pos = player:get_pos()
+		if not fly_check(player) then
+			if oldpos[name] then
+				player:set_pos(oldpos[name])
+			end
+			minetest.kick_player(player:get_player_name(), "Aratox caught you cheating! (AntiCheat)")
+
+			if not oldpos[name] then oldpos[name] = pos end
+			minetest.after(3, posdel, player)
+		end
+	end
+end)
