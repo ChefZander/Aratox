ad5312960575c8c58ed26d67c3839b19b5570e5c
Zander200-glitch <60350322+Zander200-glitch@users.noreply.github.com>
2021-04-17 15:55:19 +0200

    add noclip base

---
+++ checks/noclip-1.lua
@@ -0,0 +1,8 @@
+minetest.register_globalstep(function(dtime)
+	local players = minetest.get_connected_players()
+	for _, player in ipairs(players) do
+		local name = player:get_player_name()
+		local pos = player:get_pos()
+		
+	end
+end)
